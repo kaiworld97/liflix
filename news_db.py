b4cbe4be4a850cce3caf248f7de924e2e1daf481
hong <hcy1997@naver.com>
2022-02-07 05:55:06 +0900

    수정 - main.html main.css main.js 수정 슬라이드 추가 백엔드에서 받은 데이터 넣는거 가능 base.html 홈버튼 기능 추가

--- news_db.py
+++ news_db.py
@@ -2,6 +2,7 @@ import os
 import django
 import csv
 import sys
+import random
 
 # 프로젝트 이름.settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "liflix.settings")
@@ -30,3 +31,9 @@ from news.models import *  # django.setup() 이후에 임포트해야 오류가 
 #     for i in hi.get_similar_movie():
 #         print(type(i))
 # view_news_data()
+
+def load_cate():
+    news = random.sample(list(NewsModel.objects.all()),3)
+    print(news)
+#
+load_cate()
