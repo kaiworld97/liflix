3a4e3fa21c9f547121286adba84e7390a85bfeaa
hong <hcy1997@naver.com>
2022-02-06 22:46:12 +0900

    수정 - base.css base.html detail.html movie_detail.css movie/view.py 추가 - movie_list.css

--- make_movie_db.py
+++ make_movie_db.py
@@ -10,26 +10,27 @@ django.setup()
 from movie.models import *  # django.setup() 이후에 임포트해야 오류가 나지 않음
 
 # csv파일 경로
-CSV_PATH_PRODUCTS = 'movie_movie.csv'
-with open(CSV_PATH_PRODUCTS, encoding='UTF8') as in_file:
-    data_reader = csv.reader(in_file)
-    next(data_reader, None)  # 출력시 함께 출력되는 맨첫줄을 제외하고 출력하기 위함
-    for row in data_reader:
-        movieModel = MovieModel()
-        movieModel.code = row[0]
-        movieModel.title = row[1]
-        movieModel.director = row[3]
-        movieModel.poster = row[5]
-        movieModel.trailer = row[6]
-        movieModel.story = row[8]
-        movieModel.set_genre(row[2])
-        movieModel.set_actor(row[4])
-        movieModel.set_similar(row[7])
-        movieModel.save()
+# CSV_PATH_PRODUCTS = 'movie_movie.csv'
+# with open(CSV_PATH_PRODUCTS, encoding='UTF8') as in_file:
+#     data_reader = csv.reader(in_file)
+#     next(data_reader, None)  # 출력시 함께 출력되는 맨첫줄을 제외하고 출력하기 위함
+#     for row in data_reader:
+#         movieModel = MovieModel()
+#         movieModel.code = row[0]
+#         movieModel.title = row[1]
+#         movieModel.director = row[3]
+#         movieModel.poster = row[5]
+#         movieModel.trailer = row[6]
+#         movieModel.story = row[8]
+#         movieModel.set_genre(row[2])
+#         movieModel.set_actor(row[4])
+#         movieModel.set_similar(row[7])
+#         movieModel.save()
 
 # json화 된 파일 불러오기
 # def view_movie_data():
 #     hi = MovieModel.objects.get(code=171539)
-#     print(hi.get_actor())
-#     print(hi.get_genre())
+#     for i in hi.get_similar():
+#         print(type(i))
+#     # print(hi.get_genre())
 # view_movie_data()
